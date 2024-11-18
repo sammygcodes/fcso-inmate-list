97ee054922454421476027ef1c35d1c939699c97
sammygcodes <samushafer@gmail.com>
2024-11-17 20:00:36 -0500

    now writes to excel spreadsheet

--- inmate.js
+++ inmate.js
@@ -1,49 +1,62 @@
-// import * as readline from 'node:readline';
-// import { stdin as input, stdout as output } from 'node:process';
+import * as readline from 'node:readline';
+import { stdin as input, stdout as output } from 'node:process';
+import { COOKIE_VALUE, TAKE_VALUE, XSFR_REGEX } from './constants.js'
 
-// const rl = readline.createInterface({ input, output });
+import * as XLSX from 'xlsx/xlsx.mjs';
+import * as fs from 'fs';
+XLSX.set_fs(fs);
 
-// rl.question('What is your name? ', (name) => {
-//   console.log(`Hello, ${name}!`);
-//   rl.close();
-// });
-
-// regular expression to filter out XSFR Token
-const XSFR_REGEX = /(?<=\=)(.*?)(?=\;)/;
-
-// this looks to be constant... praying it stays that way
-const cookieValue = '.AspNetCore.Antiforgery.9fXoN5jHCXs=CfDJ8Ce9VDaDKHpIptOWN3yolAThzCBrYRqo9xrMZSfLv9CCmAixLynEStAIOIinalmNRW5JfmFAu0Q5JwxiV3uBEwaMiaQ6Oqux3aS1KOf5WhkUrlMxPQB5FHLgvsr4av-3KexgruwTOCgjrQBsKUFqsio';
+const rl = readline.createInterface({ input, output });
 
 // value needed for API call to work
 let XSFR;
 
 let totalInmates = -1;
-let maxBond = -1;
+let maxBond = 0;
 
-// amount of inmates to retrieve per API call
-const TAKE_VALUE = 10;
 let skipValue = 0;
 
 let inmateList = [];
 let filteredList = [];
 
-// get XSFR value from Ads Settings Call
-await getAdsData();
+rl.setPrompt('What is the maximum bond amount? (no comma separator): ');
+rl.prompt();
 
-// retrieve all inmates (API only retrieves 10 at a time on browser)
-do {
-    let partialInmateList = await getInmateData(TAKE_VALUE, skipValue, skipValue === 0);
-    addInmates(partialInmateList);
-    skipValue += TAKE_VALUE;
-} while (skipValue < 30);
+rl.on('line', async function(amount) {
+    if(amount > 0) {
+        maxBond = amount;
+        console.log('Getting list...');
+        await getInmates();
+        exportList();
+        rl.close();
+    } else {
+        console.log('Invalid amount. Try again.');
+        rl.prompt();
+    }
+}).on('close', function() {
+    process.exit(0);
+});
 
-processList(5000);
+async function getInmates() {
+    // get XSFR value from Ads Settings Call
+    await getAdsData();
+
+    // retrieve all inmates (API only retrieves 10 at a time on browser)
+    do {
+        let partialInmateList = await getInmateData(TAKE_VALUE, skipValue, !skipValue);
+        addInmates(partialInmateList);
+        skipValue += TAKE_VALUE;
+    } while (skipValue < 30);
+
+    processList(maxBond);
+}
 
 function addInmates(partialInmateList) {
     partialInmateList.forEach((inmate) => {
         inmateList.push(inmate);
     });
-    console.log(inmateList.length + ' inmates in list');
+    //console.log(inmateList.length + ' inmates in list');
+    console.log(`${inmateList.length}/${totalInmates} inmates retrieved`)
 }
 
 function processList(bondAmount) {
@@ -52,7 +65,24 @@ function processList(bondAmount) {
             filteredList.push(inmate);
         }
     });
-    console.log('filtered size: ' + filteredList.length);
+    console.log(`There are ${filteredList.length} inmates with a total bond amount under $${maxBond}`);
+}
+
+function exportList() {
+    console.log('Exporting list...');
+    let csvList = [];
+    filteredList.forEach((inmate) => {
+        csvList.push({
+            firstName: inmate.FirstName,
+            lastName: inmate.LastName,
+            totalBondAmount: inmate.TotalBondAmount
+        });
+    });
+
+    const sheet = XLSX.utils.json_to_sheet(csvList);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, sheet, "Inmates");
+    XLSX.writeFile(workbook, 'test.xlsx');
 }
 
 async function getAdsData() {
@@ -70,7 +100,7 @@ async function getAdsData() {
             "sec-fetch-mode": "cors",
             "sec-fetch-site": "same-origin",
             //"traceparent": "00-63055eccc64c47ff8a5f706b9ed60145-d26f7bfa3c3e4ebf-01",
-            "cookie": cookieValue,
+            "cookie": COOKIE_VALUE,
             "Referer": "https://forsythsheriffnc.policetocitizen.com/Inmates/Catalog",
             "Referrer-Policy": "strict-origin-when-cross-origin"
             },
@@ -108,7 +138,7 @@ async function getInmateData(start, skip, includeCount) {
                 "sec-fetch-site": "same-origin",
                 //"traceparent": "00-63055eccc64c47ff8a5f706b9ed60145-a487134d0c8b4bde-01",
                 "x-xsrf-token": XSFR,
-                "cookie": cookieValue,
+                "cookie": COOKIE_VALUE,
                 "Referer": "https://forsythsheriffnc.policetocitizen.com/Inmates/Catalog",
                 "Referrer-Policy": "strict-origin-when-cross-origin"
             },
